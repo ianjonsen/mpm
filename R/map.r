ca6922928c95769121bd77951fcc341100076ba1
ianjonsen <ian.jonsen@mq.edu.au>
2018-03-11 14:46:07 +1100

    landmass colour is an argument; filter wm on lat extent to speed plotting

--- R/map.r
+++ R/map.r
@@ -9,14 +9,15 @@
 ##' @importFrom mapproj mapproject
 ##' @importFrom viridis scale_colour_viridis
 ##' @export
-map <- function(m, proj = "mercator", params = NULL, xlim = NULL, ylim = NULL, theme = "dark") {
+map <- function(m, proj = "mercator", params = NULL, xlim = NULL, ylim = NULL, theme = "dark", landcol = grey(0.95)) {
 
-countriesHigh <- NULL
-data(countriesHigh, package = "rworldxtra", envir = environment())
-wm <- suppressMessages(fortify(countriesHigh))
+  if(is.null(xlim)) xlim <- extendrange(m$data$lon, f = 0.04)
+  if(is.null(ylim)) ylim <- extendrange(m$data$lat, f = 0.04)
 
-if(is.null(xlim)) xlim <- extendrange(m$data$lon, f = 0.04)
-if(is.null(ylim)) ylim <- extendrange(m$data$lat, f = 0.04)
+  countriesHigh <- NULL
+  data(countriesHigh, package = "rworldxtra", envir = environment())
+  wm <- suppressMessages(fortify(countriesHigh)) %>%
+      filter(., lat <= ylim[2]) ## FIXME: this is biased to S hemisphere...
 
 p <-
   ggplot() +
@@ -28,29 +29,29 @@ p <- p +
              data = m$data,
              size = 0.5) +
   viridis::scale_colour_viridis(name = expression(italic(gamma[t])), begin = 0, end = 1, direction = -1) +
-  switch(theme,
-         dark = theme_dark(),
-         none = theme_void(),
-         light = theme_light(),
-         minimal = theme_minimal()
-  )
+    if(theme == "dark") { theme_dark() }
+else if(theme == "minimal") { theme_minimal() }
+else if(theme == "void") { theme_void() }
+else if(theme == "light") { theme_light() }
+else if(theme == "classic") { theme_classic() }
 
-if(proj == "lambert") {
-  p <- p +
-  theme(axis.title.x = element_blank(),
-        axis.text.x = element_blank(),
-        axis.ticks.x = element_blank()) +
-  theme(axis.title.y = element_blank(),
-        axis.text.y = element_blank(),
-        axis.ticks.y = element_blank()) +
-  theme(legend.title = element_text(hjust = 0.5))
-  }
+
+ if(proj == "lambert") {
+   p <- p +
+#   theme(axis.title.x = element_blank(),
+#         axis.text.x = element_blank(),
+#         axis.ticks.x = element_blank()) +
+   theme(axis.title.y = element_blank(),
+         axis.text.y = element_blank(),
+         axis.ticks.y = element_blank()) +
+   theme(legend.title = element_text(hjust = 0.5))
+   }
 
 p <- p +
   geom_polygon(
     data = wm,
     aes_string(x = "long", y = "lat", group = "group"),
-    fill = grey(0.95)
+    fill = landcol
   )
 
 p
