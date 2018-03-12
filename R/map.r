e3dc0c5b86c5b1211877ad91229401d6d2bf84fa
ianjonsen <ian.jonsen@mq.edu.au>
2018-03-13 08:19:20 +1100

    added orientation argument for proj=ortho; added FIXME notes for future improvements

--- R/map.r
+++ R/map.r
@@ -9,34 +9,56 @@
 ##' @importFrom mapproj mapproject
 ##' @importFrom viridis scale_colour_viridis
 ##' @export
-map <- function(m, proj = "mercator", params = NULL, xlim = NULL, ylim = NULL, theme = "dark", landcol = grey(0.95)) {
+map <-
+  function(m,
+           proj = "mercator",
+           params = NULL,
+           orientation = NULL,
+           xlim = NULL,
+           ylim = NULL,
+           theme = "dark",
+           landcol = grey(0.95)) {
+
 
   if(is.null(xlim)) xlim <- extendrange(m$data$lon, f = 0.04)
   if(is.null(ylim)) ylim <- extendrange(m$data$lat, f = 0.04)
 
   countriesHigh <- NULL
   data(countriesHigh, package = "rworldxtra", envir = environment())
+  ## filter out unneeded landmasses to speed plotting
+  ##  FIXME: fortify() will be deprecated in ggplot2 - replace w appropriate broom:: fn
   wm <- suppressMessages(fortify(countriesHigh)) %>%
       filter(., lat <= ylim[2]) ## FIXME: this is biased to S hemisphere...
 
 p <-
   ggplot() +
-  coord_map(projection = proj, parameters = params, xlim = xlim, ylim = ylim) +
+  coord_map(
+    projection = proj,
+    parameters = params,
+    orientation = orientation,
+    xlim = xlim,
+    ylim = ylim
+  ) +
   xlab("Longitude") + ylab("Latitude")
 
 p <- p +
   geom_point(aes(x = lon, y = lat, colour = m$fitted$g),
              data = m$data,
              size = 0.5) +
-  viridis::scale_colour_viridis(name = expression(italic(gamma[t])), begin = 0, end = 1, direction = -1) +
+  viridis::scale_colour_viridis(
+    name = expression(italic(gamma[t])),
+    begin = 0,
+    end = 1,
+    direction = -1
+  ) +
     if(theme == "dark") { theme_dark() }
 else if(theme == "minimal") { theme_minimal() }
 else if(theme == "void") { theme_void() }
 else if(theme == "light") { theme_light() }
 else if(theme == "classic") { theme_classic() }
+## FIXME: should we go w "void" & use Sumner's graticule() here? at least for conic projections
 
-
- if(proj == "lambert") {
+ if(proj != "mercator") {
    p <- p +
 #   theme(axis.title.x = element_blank(),
 #         axis.text.x = element_blank(),
